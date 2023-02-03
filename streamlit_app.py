ea80adb2c3cf6c25b7f0cf416b8273afc51ead14
krishanus <krishanus@gmail.com>
2023-02-03 10:14:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-
-
+streamlit.title('Buttercreme')
+streamlit.header('Cakes')
+streamlit.text('Blackforest')
+streamlit.text('Princess')
