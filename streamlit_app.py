000a863364f9bcb0995471a32d7e48c564f726dc
krishanus <krishanus@gmail.com>
2023-02-03 10:17:52 +0100

    updates
    updated Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,11 @@ streamlit.title('Buttercreme')
 streamlit.header('Cakes')
 streamlit.text('Blackforest')
 streamlit.text('Princess')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Design 🥝🍇')
